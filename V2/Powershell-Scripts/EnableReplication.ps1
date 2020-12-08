ed94605d6a60c0a1318d1cbb40144c459e7de63a
Om Nishant <omnishan@microsoft.com>
2020-12-09 01:02:37 +0530

    Removing hardcoded values

--- V2/Powershell-Scripts/EnableReplication.ps1
+++ V2/Powershell-Scripts/EnableReplication.ps1
@@ -13,19 +13,6 @@
     [string] $RecoveryTargetDiskAccountType = 'Standard_LRS'
 )
 
-$VaultSubscriptionId = "605e5f88-99d5-4be1-965d-445852415039"
-$vaultResourceGroupName = "omn-2010-ccy"
-$vaultName = "omn-2010-ccy-01"
-$primaryRegion = "CentralUS"
-$recoveryRegion = "WestUs"
-$policyName = "AzureToAzureReplicationPolicy"
-$sourceVmARMIdsCSV = "/subscriptions/605e5f88-99d5-4be1-965d-445852415039/resourceGroups/omn-templateresources-CUS/providers/Microsoft.Compute/virtualMachines/omn-templateVM-11"
-$TargetResourceGroupId = "/subscriptions/605e5f88-99d5-4be1-965d-445852415039/resourceGroups/omn-wus-templateDeployedVMs"
-$PrimaryStagingStorageAccount = "/subscriptions/605e5f88-99d5-4be1-965d-445852415039/resourceGroups/omn-templateresources-CUS/providers/Microsoft.Storage/storageAccounts/omncusstorageaccount"
-$RecoveryReplicaDiskAccountType = "Standard_LRS"
-$RecoveryTargetDiskAccountType = "Standard_LRS"
-$TargetVirtualNetworkId = "/subscriptions/605e5f88-99d5-4be1-965d-445852415039/resourceGroups/omn-wus-templateDeployedVMs/providers/Microsoft.Network/virtualNetworks/omn-wus-templateDeployedVMs-vnet"
-
 # Initialize the designated output of deployment script that can be accessed by various scripts in the template.
 $DeploymentScriptOutputs = @{}
 $sourceVmARMIds = $sourceVmARMIdsCSV.Split(',')
