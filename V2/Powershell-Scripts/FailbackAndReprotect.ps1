8ab1be0323b1c01f8c0a94319007a3869c724960
Om Nishant <omnishan@microsoft.com>
2021-01-13 13:15:40 +0530

    Fixing recovery resource group ID for 2nd reprotect

--- V2/Powershell-Scripts/FailbackAndReprotect.ps1
+++ V2/Powershell-Scripts/FailbackAndReprotect.ps1
@@ -4,9 +4,9 @@
     [string] $VaultName,
     [string] $PrimaryRegion,
     [string] $RecoveryRegion,
-	[string] $SourceVmARMIdsCSV,
+    [string] $SourceVmARMIdsCSV,
     [string] $DrVMArmIdsCSV,
-	[string] $PrimaryStagingStorageAccount,
+    [string] $PrimaryStagingStorageAccount,
     [string] $RecoveryReplicaDiskAccountType = 'Standard_LRS',
     [string] $RecoveryTargetDiskAccountType = 'Standard_LRS')
 
@@ -50,24 +50,24 @@ $rpisInContainer
 $failoverJobs = New-Object System.Collections.ArrayList
 $rpiLookUpByJobId = @{}
 foreach ($rpi in $rpisInContainer) {
-	# Trigger Failover.
-	$message = 'Triggering failover for {0}.' -f $rpi.FriendlyName
-	Write-Output $message
-	$job = Start-ASRFO -ReplicationProtectedItem $rpi -Direction PrimaryToRecovery
-	$failoverJobs.Add($job)
-	$rpiLookUpByJobId[$job.Id] = $rpi
+    # Trigger Failover.
+    $message = 'Triggering failover for {0}.' -f $rpi.FriendlyName
+    Write-Output $message
+    $job = Start-ASRFO -ReplicationProtectedItem $rpi -Direction PrimaryToRecovery
+    $failoverJobs.Add($job)
+    $rpiLookUpByJobId[$job.Id] = $rpi
 }
 
 $failoverCommitJobs = New-Object System.Collections.ArrayList
 
 foreach ($job in $failoverJobs) {
-	do {
-		Start-Sleep -Seconds 50
-		$job = Get-AsrJob -Job $job
-		Write-Output $job.State
-	} while ($job.State -ne 'Succeeded' -and $job.State -ne 'Failed' -and $job.State -ne 'CompletedWithInformation')
-	
-	if ($job.State -eq 'Failed') {
+    do {
+        Start-Sleep -Seconds 50
+        $job = Get-AsrJob -Job $job
+        Write-Output $job.State
+    } while ($job.State -ne 'Succeeded' -and $job.State -ne 'Failed' -and $job.State -ne 'CompletedWithInformation')
+    
+    if ($job.State -eq 'Failed') {
        $message = 'Job {0} failed for {1}' -f $job.DisplayName, $job.TargetObjectName
        Write-Output $message
        foreach ($er in $job.Errors) {
@@ -83,71 +83,71 @@ foreach ($job in $failoverJobs) {
        throw $message
     }
 
-	$message = 'Failover completed for {0} with state {1}. Starting commit FO.' -f $job.TargetObjectName, $job.State
-	Write-Output $message
-	$rpi = $rpiLookUpByJobId[$job.ID]
-	$commitJob = Start-ASRCommitFailover -ReplicationProtectedItem $rpi
-	$failoverCommitJobs.Add($commitJob)
-	$rpiLookUpByJobId[$commitJob.Id] = $rpi
+    $message = 'Failover completed for {0} with state {1}. Starting commit FO.' -f $job.TargetObjectName, $job.State
+    Write-Output $message
+    $rpi = $rpiLookUpByJobId[$job.ID]
+    $commitJob = Start-ASRCommitFailover -ReplicationProtectedItem $rpi
+    $failoverCommitJobs.Add($commitJob)
+    $rpiLookUpByJobId[$commitJob.Id] = $rpi
 }
 
 $reverseReplicationJobs = New-Object System.Collections.ArrayList
 
 foreach ($job in $failoverCommitJobs) {
-	do {
-		Start-Sleep -Seconds 50
-		$job = Get-AsrJob -Job $job
-		Write-Output $job.State
-	} while ($job.State -ne 'Succeeded' -and $job.State -ne 'Failed' -and $job.State -ne 'CompletedWithInformation')
+    do {
+        Start-Sleep -Seconds 50
+        $job = Get-AsrJob -Job $job
+        Write-Output $job.State
+    } while ($job.State -ne 'Succeeded' -and $job.State -ne 'Failed' -and $job.State -ne 'CompletedWithInformation')
 
-	$rpi = $rpiLookUpByJobId[$job.ID]
-	$ProtectedItemName = $rpi.FriendlyName
-	$message = 'Committed Failover for {0}.' -f $ProtectedItemName
-	Write-Output ''
-	
-	$sourceVmResourceGroupId = $rpi.ProviderSpecificDetails.RecoveryAzureResourceGroupId
-	$sourceVmResourceGroupName = $sourceVmResourceGroupId.Split('/')[4]
-	$sourceVM = Get-AzVM -ResourceGroupName $sourceVmResourceGroupName -Name $ProtectedItemName
+    $rpi = $rpiLookUpByJobId[$job.ID]
+    $ProtectedItemName = $rpi.FriendlyName
+    $message = 'Committed Failover for {0}.' -f $ProtectedItemName
+    Write-Output ''
+    
+    $sourceVmResourceGroupId = $rpi.ProviderSpecificDetails.RecoveryAzureResourceGroupId
+    $sourceVmResourceGroupName = $sourceVmResourceGroupId.Split('/')[4]
+    $sourceVM = Get-AzVM -ResourceGroupName $sourceVmResourceGroupName -Name $ProtectedItemName
 
-	$message = 'Reverse replication to be triggered for {0}' -f $sourceVM.ID
-	Write-Output $message
-	$currentVmArmId = $rpi.ProviderSpecificDetails.FabricObjectId
-	$currentVmResourceGroupId = $currentVmArmId.Substring(0, $SourceVmArmId.ToLower().IndexOf('/providers'))
+    $message = 'Reverse replication to be triggered for {0}' -f $sourceVM.ID
+    Write-Output $message
+    $currentVmArmId = $rpi.ProviderSpecificDetails.FabricObjectId
+    $currentVmResourceGroupId = $currentVmArmId.Substring(0, $currentVmArmId.ToLower().IndexOf('/providers'))
 
-	$message = 'Storage account to be used {0}' -f $PrimaryStagingStorageAccount
-	Write-Output $message
-	
-	# Prepare disk configuration.
-	$diskList =  New-Object System.Collections.ArrayList
-	$osDisk =	New-AzRecoveryServicesAsrAzureToAzureDiskReplicationConfig -DiskId $sourceVM.StorageProfile.OsDisk.ManagedDisk.Id `
-		-LogStorageAccountId $PrimaryStagingStorageAccount -ManagedDisk  -RecoveryReplicaDiskAccountType $RecoveryReplicaDiskAccountType `
-		-RecoveryResourceGroupId  $currentVmResourceGroupId -RecoveryTargetDiskAccountType $RecoveryTargetDiskAccountType          
-	$diskList.Add($osDisk)
-	
-	foreach($dataDisk in $sourceVM.StorageProfile.DataDisks)
-	{
-		$disk = New-AzRecoveryServicesAsrAzureToAzureDiskReplicationConfig -DiskId $dataDisk.ManagedDisk.Id `
-			-LogStorageAccountId $PrimaryStagingStorageAccount -ManagedDisk  -RecoveryReplicaDiskAccountType $RecoveryReplicaDiskAccountType `
-			-RecoveryResourceGroupId  $currentVmResourceGroupId -RecoveryTargetDiskAccountType $RecoveryTargetDiskAccountType
-		$diskList.Add($disk)
-	}
-	
-	$message = 'Reverse replication being triggered'
-	Write-Output $message
-	$reverseReplciationJob = Update-AzRecoveryServicesAsrProtectionDirection -AzureToAzure -LogStorageAccountId $PrimaryStagingStorageAccount  -ProtectionContainerMapping 			$reverseContainerMapping  -RecoveryResourceGroupId $currentVmResourceGroupId -ReplicationProtectedItem $rpi
-	$reverseReplicationJobs.Add($reverseReplciationJob)	
+    $message = 'Storage account to be used {0}' -f $PrimaryStagingStorageAccount
+    Write-Output $message
+    
+    # Prepare disk configuration.
+    $diskList =  New-Object System.Collections.ArrayList
+    $osDisk =    New-AzRecoveryServicesAsrAzureToAzureDiskReplicationConfig -DiskId $sourceVM.StorageProfile.OsDisk.ManagedDisk.Id `
+        -LogStorageAccountId $PrimaryStagingStorageAccount -ManagedDisk  -RecoveryReplicaDiskAccountType $RecoveryReplicaDiskAccountType `
+        -RecoveryResourceGroupId  $currentVmResourceGroupId -RecoveryTargetDiskAccountType $RecoveryTargetDiskAccountType          
+    $diskList.Add($osDisk)
+    
+    foreach($dataDisk in $sourceVM.StorageProfile.DataDisks)
+    {
+        $disk = New-AzRecoveryServicesAsrAzureToAzureDiskReplicationConfig -DiskId $dataDisk.ManagedDisk.Id `
+            -LogStorageAccountId $PrimaryStagingStorageAccount -ManagedDisk  -RecoveryReplicaDiskAccountType $RecoveryReplicaDiskAccountType `
+            -RecoveryResourceGroupId  $currentVmResourceGroupId -RecoveryTargetDiskAccountType $RecoveryTargetDiskAccountType
+        $diskList.Add($disk)
+    }
+    
+    $message = 'Reverse replication being triggered'
+    Write-Output $message
+    $reverseReplciationJob = Update-AzRecoveryServicesAsrProtectionDirection -AzureToAzure -LogStorageAccountId $PrimaryStagingStorageAccount  -ProtectionContainerMapping             $reverseContainerMapping  -RecoveryResourceGroupId $currentVmResourceGroupId -ReplicationProtectedItem $rpi
+    $reverseReplicationJobs.Add($reverseReplciationJob)    
 }
 
 foreach ($job in $reverseReplicationJobs) {
-	$targetObjectName = $job.TargetObjectName
+    $targetObjectName = $job.TargetObjectName
 
-	do {
-		Start-Sleep -Seconds 50
-		$job = Get-AsrJob -Job $job
-		Write-Output $job.State
-	} while ($job.State -ne 'Succeeded' -and $job.State -ne 'Failed' -and $job.State -ne 'CompletedWithInformation')
-	
-	if ($job.State -eq 'Failed') {
+    do {
+        Start-Sleep -Seconds 50
+        $job = Get-AsrJob -Job $job
+        Write-Output $job.State
+    } while ($job.State -ne 'Succeeded' -and $job.State -ne 'Failed' -and $job.State -ne 'CompletedWithInformation')
+    
+    if ($job.State -eq 'Failed') {
        $message = 'Job {0} failed for {1}' -f $job.DisplayName, $job.TargetObjectName
        Write-Output $message
        foreach ($er in $job.Errors) {
@@ -162,29 +162,29 @@ foreach ($job in $reverseReplicationJobs) {
 
        throw $message
     }
-    	
-	$message = 'Reverse replication completed for {0}. Waiting for IR.' -f $targetObjectName
-	Write-Output $message
-	
-	$startTime = $job.StartTime
-	$irFinished = $false
-	do 
-	{
-		$irJobs = Get-ASRJob | where {$_.JobType -like '*IrCompletion' -and $_.TargetObjectName -eq $targetObjectName -and $_.StartTime -gt $startTime} | Sort-Object StartTime -Descending | select -First 2  
-		if ($irJobs -ne $null -and $irJobs.Length -ne $0) {
-			$secondaryIrJob = $irJobs | where {$_.JobType -like 'SecondaryIrCompletion'}
-			if ($secondaryIrJob -ne $null -and $secondaryIrJob.Length -ge $1) {
-				$irFinished = $secondaryIrJob.State -eq 'Succeeded' -or $secondaryIrJob.State -eq 'Failed'
-			}
-			else {
-				$irFinished = $irJobs.State -eq 'Failed'
-			}
-		}
-	
-		if (-not $irFinished) {
-			Start-Sleep -Seconds 50
-		}
-	} while (-not $irFinished)
+
+    $message = 'Reverse replication completed for {0}. Waiting for IR.' -f $targetObjectName
+    Write-Output $message
+    
+    $startTime = $job.StartTime
+    $irFinished = $false
+    do 
+    {
+        $irJobs = Get-ASRJob | where {$_.JobType -like '*IrCompletion' -and $_.TargetObjectName -eq $targetObjectName -and $_.StartTime -gt $startTime} | Sort-Object StartTime -Descending | select -First 2  
+        if ($irJobs -ne $null -and $irJobs.Length -ne $0) {
+            $secondaryIrJob = $irJobs | where {$_.JobType -like 'SecondaryIrCompletion'}
+            if ($secondaryIrJob -ne $null -and $secondaryIrJob.Length -ge $1) {
+                $irFinished = $secondaryIrJob.State -eq 'Succeeded' -or $secondaryIrJob.State -eq 'Failed'
+            }
+            else {
+                $irFinished = $irJobs.State -eq 'Failed'
+            }
+        }
+    
+        if (-not $irFinished) {
+            Start-Sleep -Seconds 50
+        }
+    } while (-not $irFinished)
 }
 
 $rpisInContainer = Get-ASRReplicationProtectedItem -ProtectionContainer $recContainer | where {$sourceVmARMIds -contains $_.ProviderSpecificDetails.FabricObjectId}
